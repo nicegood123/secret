779c7ed10cec1737cc2922e61c53ad8715389499
Gisan Geff <ggranraniego@gmail.com>
2020-12-06 09:57:07 +0800

    Add product description to product table

--- pos.sql
+++ pos.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2017 at 10:51 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Generation Time: Dec 06, 2020 at 02:55 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -33,6 +35,13 @@ CREATE TABLE `cart` (
   `qty` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cartid`, `userid`, `productid`, `qty`) VALUES
+(15, 2, 8, 6);
+
 -- --------------------------------------------------------
 
 --
@@ -50,7 +59,7 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`categoryid`, `category_name`) VALUES
 (1, 'Laptops'),
-(2, 'Desktop PC''s'),
+(2, 'Desktop PC\'s'),
 (3, 'Tablets');
 
 -- --------------------------------------------------------
@@ -71,7 +80,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`userid`, `customer_name`, `address`, `contact`) VALUES
-(2, 'Neovic Devierte', 'Silay City, Negros Occidental', '09092735719');
+(2, 'Neovic Devierte', 'Silay City, Negros Occidental', '09092735719'),
+(13, 'Abdul Salsalani', 'Block 12 Lot 12 Matina Aplaya, Davao City', '+639123789168');
 
 -- --------------------------------------------------------
 
@@ -102,7 +112,13 @@ INSERT INTO `inventory` (`inventoryid`, `userid`, `action`, `productid`, `quanti
 (7, 2, 'Purchase', 3, 12, '2017-09-18 11:09:22'),
 (8, 2, 'Purchase', 4, 8, '2017-09-18 11:09:22'),
 (9, 1, 'Add Product', 27, 10, '2017-09-18 13:59:25'),
-(10, 1, 'Update quantity', 27, 20, '2017-09-18 14:04:32');
+(10, 1, 'Update quantity', 27, 20, '2017-09-18 14:04:32'),
+(11, 1, 'Add Product', 28, 5, '2020-11-27 03:44:09'),
+(12, 2, 'Purchase', 12, 2, '2020-11-30 14:21:26'),
+(13, 2, 'Purchase', 28, 2, '2020-12-06 07:11:55'),
+(14, 1, 'Add Product', 29, 2, '2020-12-06 08:40:10'),
+(15, 4, 'Add Product', 30, 11, '2020-12-06 08:49:26'),
+(16, 4, 'Add Product', 31, 7, '2020-12-06 08:51:15');
 
 -- --------------------------------------------------------
 
@@ -114,6 +130,7 @@ CREATE TABLE `product` (
   `productid` int(11) NOT NULL,
   `categoryid` int(11) NOT NULL,
   `product_name` varchar(150) NOT NULL,
+  `product_description` text NOT NULL,
   `product_price` double NOT NULL,
   `product_qty` double NOT NULL,
   `photo` varchar(200) NOT NULL,
@@ -124,28 +141,30 @@ CREATE TABLE `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`productid`, `categoryid`, `product_name`, `product_price`, `product_qty`, `photo`, `supplierid`) VALUES
-(1, 1, 'DELL Inspiron 15 7000 15.6', 899, 891, 'upload/1.jpg', 4),
-(2, 1, 'MICROSOFT Surface Pro 4 & Typecover - 128 GB\r\n', 799, 948, 'upload/2.jpg', 0),
-(3, 2, 'DELL Inspiron 15 5000 15.6', 599, 977, 'upload/3.jpg', 4),
-(4, 1, 'LENOVO Ideapad 320s-14IKB 14" Laptop - Grey', 399, 982, 'upload/4.jpg', 0),
-(5, 1, 'APPLE MacBook Air 13.3" (2017)', 879, 9900, 'upload/5.jpg', 0),
-(6, 1, 'DELL Inspiron 15 5000 15', 449.99, 1000, 'upload/6.jpg', 4),
-(8, 1, 'ASUS Transformer Mini T102HA 10.1" 2 in 1 - Silver', 549.99, 1000, 'upload/8.jpg', 0),
-(9, 2, 'PC SPECIALIST Vortex Core Lite Gaming PC', 599.99, 1000, 'upload/9.jpg', 0),
-(10, 2, 'DELL Inspiron 5675 Gaming PC - Recon Blue', 599.99, 1000, 'upload/10.jpg', 4),
-(11, 2, 'HP Barebones OMEN X 900-099nn Gaming PC', 489.98, 1000, 'upload/11.jpg', 0),
-(12, 2, 'ACER Aspire GX-781 Gaming PC', 749.99, 1000, 'upload/12.jpg', 0),
-(13, 2, 'HP Pavilion Power 580-015na Gaming PC', 799.99, 1000, 'upload/13.jpg', 0),
-(14, 2, 'LENOVO Legion Y520 Gaming PC', 899.99, 1000, 'upload/14.jpg', 0),
-(15, 2, 'PC SPECIALIST Vortex Minerva XT-R Gaming PC', 999.99, 1000, 'upload/15.jpg', 0),
-(16, 2, 'C SPECIALIST Vortex Core II Gaming PC', 649.99, 1000, 'upload/16.jpg', 0),
-(17, 3, 'AMAZON Fire 7 Tablet with Alexa (2017) - 8 GB, Black', 49.99, 1000, 'upload/17.jpg', 0),
-(18, 3, 'AMAZON Fire HD 8 Tablet with Alexa (2017) - 16 GB, Black', 79.99, 1000, 'upload/18.jpg', 0),
-(19, 3, 'AMAZON Fire HD 8 Tablet with Alexa (2017) - 32 GB, Black', 99.99, 1000, 'upload/19.jpg', 0),
-(20, 3, 'APPLE 9.7" iPad - 32 GB, Space Grey', 339, 990, 'upload/20.jpg', 0),
-(21, 3, 'APPLE 9.7" iPad - 32 GB, Gold', 339, 1000, 'upload/21.jpg', 0),
-(22, 3, 'APPLE 10.5" iPad Pro - 64 GB, Space Grey (2017)', 619, 1000, 'upload/22.jpg', 0);
+INSERT INTO `product` (`productid`, `categoryid`, `product_name`, `product_description`, `product_price`, `product_qty`, `photo`, `supplierid`) VALUES
+(1, 1, 'DELL Inspiron 15 7000 15.6', '0', 899, 891, 'upload/1.jpg', 4),
+(2, 1, 'MICROSOFT Surface Pro 4 & Typecover - 128 GB\r\n', '0', 1799.5, 948, 'upload/2.jpg', 0),
+(3, 2, 'DELL Inspiron 15 5000 15.6', '0', 599, 977, 'upload/3.jpg', 4),
+(4, 1, 'LENOVO Ideapad 320s-14IKB 14\" Laptop - Grey', '0', 399, 982, 'upload/4.jpg', 0),
+(5, 1, 'APPLE MacBook Air 13.3\" (2017)', '0', 879, 9900, 'upload/5.jpg', 0),
+(6, 1, 'DELL Inspiron 15 5000 15', '0', 449.99, 1000, 'upload/6.jpg', 4),
+(8, 1, 'ASUS Transformer Mini T102HA 10.1\" 2 in 1 - Silver', '0', 549.99, 1000, 'upload/8.jpg', 0),
+(9, 2, 'PC SPECIALIST Vortex Core Lite Gaming PC', '0', 599.99, 1000, 'upload/9.jpg', 0),
+(10, 2, 'DELL Inspiron 5675 Gaming PC - Recon Blue', '0', 599.99, 1000, 'upload/10.jpg', 4),
+(11, 2, 'HP Barebones OMEN X 900-099nn Gaming PC', '0', 489.98, 1000, 'upload/11.jpg', 0),
+(12, 2, 'ACER Aspire GX-781 Gaming PC', '0', 749.99, 998, 'upload/12.jpg', 4),
+(13, 2, 'HP Pavilion Power 580-015na Gaming PC', '0', 799.99, 1000, 'upload/13.jpg', 0),
+(14, 2, 'LENOVO Legion Y520 Gaming PC', '0', 899.99, 1000, 'upload/14.jpg', 0),
+(15, 2, 'PC SPECIALIST Vortex Minerva XT-R Gaming PC', '0', 999.99, 1000, 'upload/15.jpg', 0),
+(16, 2, 'C SPECIALIST Vortex Core II Gaming PC', '0', 649.99, 1000, 'upload/16.jpg', 0),
+(17, 3, 'AMAZON Fire 7 Tablet with Alexa (2017) - 8 GB, Black', '0', 49.99, 1000, 'upload/17.jpg', 0),
+(18, 3, 'AMAZON Fire HD 8 Tablet with Alexa (2017) - 16 GB, Black', '0', 79.99, 1000, 'upload/18.jpg', 0),
+(19, 3, 'AMAZON Fire HD 8 Tablet with Alexa (2017) - 32 GB, Black', '0', 99.99, 1000, 'upload/19.jpg', 0),
+(20, 3, 'APPLE 9.7\" iPad - 32 GB, Space Grey', '0', 339, 990, 'upload/20.jpg', 0),
+(21, 3, 'APPLE 9.7\" iPad - 32 GB, Gold', '0', 339, 1000, 'upload/21.jpg', 0),
+(22, 3, 'APPLE 10.5\" iPad Pro - 64 GB, Space Grey (2017)', '0', 619, 1000, 'upload/22.jpg', 0),
+(29, 3, 'Busha', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, neque nesciunt eos fuga doloremque ipsam quidem fugit eveniet id repellendus quae veniam temporibus autem nisi tenetur! Fuga facere quam autem.', 5, 2, 'upload/xr8dsrxa7tu21_1607215210.jpg', 4),
+(31, 1, 'Shabu', 'Sprilikitiwi', 50, 7, 'upload/profile_1607215875.jpg', 4);
 
 -- --------------------------------------------------------
 
@@ -174,7 +193,10 @@ INSERT INTO `sales` (`salesid`, `userid`, `sales_total`, `sales_date`) VALUES
 (7, 2, 15579, '2017-09-18 09:09:34'),
 (8, 2, 112361, '2017-09-18 09:32:00'),
 (9, 2, 7990, '2017-09-18 09:34:29'),
-(10, 2, 18370, '2017-09-18 11:09:21');
+(10, 2, 18370, '2017-09-18 11:09:21'),
+(11, 2, 1499.98, '2020-11-30 14:21:26'),
+(12, 2, 2, '2020-12-06 07:11:55'),
+(13, 13, 0, '2020-12-06 09:18:27');
 
 -- --------------------------------------------------------
 
@@ -211,7 +233,9 @@ INSERT INTO `sales_detail` (`sales_detailid`, `salesid`, `productid`, `sales_qty
 (15, 9, 2, 10),
 (16, 10, 2, 10),
 (17, 10, 3, 12),
-(18, 10, 4, 8);
+(18, 10, 4, 8),
+(19, 11, 12, 2),
+(20, 12, 28, 2);
 
 -- --------------------------------------------------------
 
@@ -253,7 +277,8 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`userid`, `username`, `password`, `access`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
 (2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 2),
-(4, 'supplier', '99b0e8da24e29e4ccb5d7d76e677c2ac', 3);
+(4, 'supplier', '99b0e8da24e29e4ccb5d7d76e677c2ac', 3),
+(13, 'salsalani', 'e10adc3949ba59abbe56e057f20f883e', 2);
 
 --
 -- Indexes for dumped tables
@@ -321,37 +346,45 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cartid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `cartid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `categoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `inventoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `inventoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `productid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `salesid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `salesid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT for table `sales_detail`
 --
 ALTER TABLE `sales_detail`
-  MODIFY `sales_detailid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `sales_detailid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
